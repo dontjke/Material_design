1b9c0bb5ae3574eb30e8c2f4c9e0b6e8f82379af
Romajke <skuaall@mail.ru>
2023-04-14 00:57:51 +0300

    добавил API запроса и файл для хранения констант/ключей

---
+++ app/src/main/java/com/example/material_design/model/PictureOfTheDayAPI.kt
@@ -0,0 +1,13 @@
+package com.example.material_design.model
+
+import com.example.material_design.utils.KEY_FOR_EXPANDED_USAGE
+import com.example.material_design.utils.RESPONSE_END_POINT
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface PictureOfTheDayAPI {
+    @GET(RESPONSE_END_POINT)
+    fun getPictureOfTheDay(@Query(KEY_FOR_EXPANDED_USAGE) apiKey:String):Call<PictureOfTheDayResponseData>
+
+}
---
+++ app/src/main/java/com/example/material_design/utils/Utils.kt
@@ -0,0 +1,4 @@
+package com.example.material_design.utils
+
+const val RESPONSE_END_POINT = "planetary/apod"
+const val KEY_FOR_EXPANDED_USAGE = "planetary/apod"
