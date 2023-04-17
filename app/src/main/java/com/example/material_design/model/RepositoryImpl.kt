9248c373b8800876cbf24417696294abe238ac42
Romajke <skuaall@mail.ru>
2023-04-17 23:26:23 +0300

    добавил ключ в properties, настройки в gradle, запрос в репозиторий с колбэком в viewmodel

--- app/src/main/java/com/example/material_design/model/RepositoryImpl.kt
+++ app/src/main/java/com/example/material_design/model/RepositoryImpl.kt
@@ -5,7 +5,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 
-class RepositoryImpl:Repository {
+class RepositoryImpl : Repository {
     private val baseUrl = "https://api.nasa.gov/"
 
     private val retrofit by lazy {
@@ -14,7 +14,7 @@ class RepositoryImpl:Repository {
             .addConverterFactory(GsonConverterFactory.create(GsonBuilder().setLenient().create()))
             .build()
     }
-    private fun getPictureOfTheDayApi():PictureOfTheDayAPI{
+    fun getPictureOfTheDayApi(): PictureOfTheDayAPI {
         return retrofit.create(PictureOfTheDayAPI::class.java)
     }
 
--- app/src/main/java/com/example/material_design/viewmodel/PictureOfTheDayViewModel.kt
+++ app/src/main/java/com/example/material_design/viewmodel/PictureOfTheDayViewModel.kt
@@ -1,8 +1,29 @@
 package com.example.material_design.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.material_design.BuildConfig
+import com.example.material_design.model.PictureOfTheDayResponseData
 import com.example.material_design.model.RepositoryImpl
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class PictureOfTheDayViewModel(val repositoryImpl: RepositoryImpl = RepositoryImpl()): ViewModel() {
+class PictureOfTheDayViewModel(private val repositoryImpl: RepositoryImpl = RepositoryImpl()): ViewModel() {
 
+    fun sendRequest(){
+        repositoryImpl.getPictureOfTheDayApi().getPictureOfTheDay(BuildConfig.NASA_API_KEY).enqueue(callback)
+    }
+
+    private val callback =  object : Callback<PictureOfTheDayResponseData>{
+        override fun onResponse(
+            call: Call<PictureOfTheDayResponseData>,
+            response: Response<PictureOfTheDayResponseData>
+        ) {
+            response.body()!!
+        }
+
+        override fun onFailure(call: Call<PictureOfTheDayResponseData>, t: Throwable) {
+            TODO("Not yet implemented")
+        }
+    }
 }
