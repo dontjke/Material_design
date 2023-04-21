88dc474fec31a9550d4b5d62470d223eab25f259
Romajke <skuaall@mail.ru>
2023-04-21 21:45:15 +0300

    добавил описание картинки

--- app/src/main/java/com/example/material_design/utils/Utils.kt
+++ app/src/main/java/com/example/material_design/utils/Utils.kt
@@ -1,5 +1,6 @@
 package com.example.material_design.utils
 
 const val RESPONSE_END_POINT = "planetary/apod"
-const val KEY_FOR_EXPANDED_USAGE = "api_key"
+const val KEY_FOR_EXPANDED_USAGE_API_KEY = "api_key"
+const val KEY_FOR_EXPANDED_USAGE_DATA = "data"
 const val BASE_URL = "https://api.nasa.gov/"
