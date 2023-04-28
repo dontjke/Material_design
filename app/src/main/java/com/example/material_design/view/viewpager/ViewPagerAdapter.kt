a156d1f5582382497d185340223e2f562f14a692
Romajke <skuaall@mail.ru>
2023-04-28 18:32:13 +0300

    убрал заголовки tabLayout

--- app/src/main/java/com/example/material_design/view/viewpager/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/material_design/view/viewpager/ViewPagerAdapter.kt
@@ -24,12 +24,7 @@ class ViewPagerAdapter (fragmentManager: FragmentManager) :
     }
 
     override fun getPageTitle(position: Int): CharSequence? {
-        return when(position){
-            EARTH_FRAGMENT-> "Earth"
-            MARS_FRAGMENT -> "Mars"
-            WEATHER_FRAGMENT -> "Weather"
-            else -> "Earth"
-        }
+        return null
     }
     companion object {
         private const val EARTH_FRAGMENT = 0
