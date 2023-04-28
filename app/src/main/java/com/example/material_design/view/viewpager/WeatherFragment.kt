dca3228eda1bf56e656898e01fcbfac02096caca
Romajke <skuaall@mail.ru>
2023-04-28 12:56:06 +0300

    добавил 3 фрагмента и макеты

---
+++ app/src/main/java/com/example/material_design/view/viewpager/WeatherFragment.kt
@@ -0,0 +1,17 @@
+package com.example.material_design.view.viewpager
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.material_design.R
+
+class WeatherFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_weather, container, false)
+    }
+}
