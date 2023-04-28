2d8972b24aa4ec8046d9c1e0ec62a59b950a14af
Romajke <skuaall@mail.ru>
2023-04-28 12:29:02 +0300

    ViewPager | добавил ViewPagerActivity и макет

---
+++ app/src/main/java/com/example/material_design/view/viewpager/ViewPagerActivity.kt
@@ -0,0 +1,15 @@
+package com.example.material_design.view.viewpager
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.material_design.databinding.ActivityViewPagerBinding
+
+class ViewPagerActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityViewPagerBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityViewPagerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+}
