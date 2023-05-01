f0012b84ae3b95678fe9146d59d531625f97ee4f
Romajke <skuaall@mail.ru>
2023-05-01 12:25:30 +0300

    переделал ViewPagerAdapter под ViewPager2

--- app/src/main/java/com/example/material_design/view/viewpager/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/material_design/view/viewpager/ViewPagerAdapter.kt
@@ -1,35 +1,36 @@
 package com.example.material_design.view.viewpager
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
 
 
-class ViewPagerAdapter (fragmentManager: FragmentManager) :
+class ViewPagerAdapter (fragmentActivity: FragmentActivity) :
 
-    FragmentStatePagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT)
+    FragmentStateAdapter(fragmentActivity)
 {
     private val fragments = arrayOf(EarthFragment(), MarsFragment(),
         WeatherFragment())
-    override fun getItem(position: Int): Fragment {
+
+    companion object {
+        private const val EARTH_FRAGMENT = 0
+        private const val MARS_FRAGMENT = 1
+        private const val WEATHER_FRAGMENT = 2
+    }
+
+    override fun getItemCount(): Int {
+        return fragments.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
         return when (position) {
             0 -> fragments[EARTH_FRAGMENT]
             1 -> fragments[MARS_FRAGMENT]
             2 -> fragments[WEATHER_FRAGMENT]
             else -> fragments[EARTH_FRAGMENT]
         }
-    }
-    override fun getCount(): Int {
-        return fragments.size
-    }
-
-    override fun getPageTitle(position: Int): CharSequence? {
-        return null
-    }
-    companion object {
-        private const val EARTH_FRAGMENT = 0
-        private const val MARS_FRAGMENT = 1
-        private const val WEATHER_FRAGMENT = 2
     }
 }
 
