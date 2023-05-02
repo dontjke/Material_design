e4bbe9b6af7664b5c4f8c7cf91dac92d25759d11
Romajke <skuaall@mail.ru>
2023-05-03 01:16:04 +0300

    попробовал RenderEffect

--- app/src/main/java/com/example/material_design/view/viewpager/EarthFragment.kt
+++ app/src/main/java/com/example/material_design/view/viewpager/EarthFragment.kt
@@ -1,9 +1,13 @@
 package com.example.material_design.view.viewpager
 
+import android.graphics.RenderEffect
+import android.graphics.Shader
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.example.material_design.R
 
@@ -15,4 +19,13 @@ class EarthFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_earth, container, false)
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val blurEffect = RenderEffect.createBlurEffect(15f, 0f,
+                Shader.TileMode.MIRROR)
+            view.findViewById<ImageView>(R.id.imageView).setRenderEffect(blurEffect)
+        }
+    }
+
 }
