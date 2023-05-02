3ea41a51b1ec2c04cebd673959382c0b27811cf6
Romajke <skuaall@mail.ru>
2023-05-03 01:19:16 +0300

    RenderEffect для всего макета

--- app/src/main/java/com/example/material_design/view/viewpager/EarthFragment.kt
+++ app/src/main/java/com/example/material_design/view/viewpager/EarthFragment.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.example.material_design.R
@@ -24,8 +25,9 @@ class EarthFragment : Fragment() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             val blurEffect = RenderEffect.createBlurEffect(15f, 0f,
                 Shader.TileMode.MIRROR)
-            view.findViewById<ImageView>(R.id.imageView).setRenderEffect(blurEffect)
+            view.findViewById<FrameLayout>(R.id.container).setRenderEffect(blurEffect)
         }
+
     }
 
 }
