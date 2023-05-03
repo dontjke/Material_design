c901d24dbb02c39a4413dbbbd41f9f016f7f2555
Romajke <skuaall@mail.ru>
2023-05-04 00:47:31 +0300

    example explode

--- app/src/main/java/com/example/material_design/view/animation/AnimationActivity.kt
+++ app/src/main/java/com/example/material_design/view/animation/AnimationActivity.kt
@@ -1,42 +1,70 @@
 package com.example.material_design.view.animation
 
+import android.graphics.Rect
 import android.os.Bundle
-import android.view.Gravity
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
-import androidx.transition.*
+import androidx.recyclerview.widget.RecyclerView
+import androidx.transition.Explode
+import androidx.transition.Transition
+import androidx.transition.TransitionManager
+import com.example.material_design.R
 import com.example.material_design.databinding.ActivityAnimationBinding
+
 
 class AnimationActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAnimationBinding
-    private var textIsVisible = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
 
-
         binding = ActivityAnimationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
-        binding.button.setOnClickListener {
+        binding.recyclerView.adapter = Adapter()
+    }
 
-            val myAutoTransition = TransitionSet()
-            //myAutoTransition.ordering = TransitionSet.ORDERING_TOGETHER
-            myAutoTransition.ordering = TransitionSet.ORDERING_SEQUENTIAL
-            val fade = Slide(Gravity.END)
-            fade.duration = 1000L
-            val changeBounds = ChangeBounds()
-            changeBounds.duration = 300L
-            myAutoTransition.addTransition(fade)
-            myAutoTransition.addTransition(changeBounds)
-            TransitionManager.beginDelayedTransition(binding.transitionsContainer,myAutoTransition)
-            textIsVisible = !textIsVisible
-            binding.text.visibility = if (textIsVisible) View.VISIBLE else View.GONE
+    private fun explode(clickedView: View) {
+        val viewRect = Rect() //создал пустой прямоугольник
+        clickedView.getGlobalVisibleRect(viewRect) //записываю координаты (слепок)
+        val explode = Explode()
+        explode.epicenterCallback = object : Transition.EpicenterCallback() {
+            override fun onGetEpicenter(transition: Transition): Rect {
+                return viewRect
+            }
+        }
+        explode.duration = 2000
+        TransitionManager.beginDelayedTransition(binding.recyclerView, explode)
+        binding.recyclerView.adapter = null
+    }
+
+    private inner class Adapter : RecyclerView.Adapter<ViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            return ViewHolder(
+                LayoutInflater.from(parent.context).inflate(
+                    R.layout.activity_animation_explode_recycle_view_item,
+                    parent,
+                    false
+                ) as View
+            )
         }
 
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.itemView.setOnClickListener {
+                explode(it)
+            }
+        }
+
+        override fun getItemCount(): Int {
+            return 32
+        }
     }
+
+    private inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
 }
