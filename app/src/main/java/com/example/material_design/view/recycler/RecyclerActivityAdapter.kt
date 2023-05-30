8513a4f2e3df5df7cba11e168b74083e39b8c158
Romajke <skuaall@mail.ru>
2023-05-30 20:32:37 +0300

    изменил onBindViewHolder. сравниваю старые данные с самыми свежими и заменяю заголовок элемента, если он изменился

--- app/src/main/java/com/example/material_design/view/recycler/RecyclerActivityAdapter.kt
+++ app/src/main/java/com/example/material_design/view/recycler/RecyclerActivityAdapter.kt
@@ -62,157 +62,163 @@ class RecyclerActivityAdapter(
         if (payloads.isEmpty())
             super.onBindViewHolder(holder, position, payloads)
         else {
-            if (payloads.any { it is Pair<*, *> })
-            holder.itemView.findViewById<TextView>(R.id.marsTextView).text =
-                data[position].first.someText
-        }
-    }
-
-    inner class EarthViewHolder(view: View) : BaseViewHolder(view) {
-        override fun bind(data: Pair<Data, Boolean>) {
-            if (layoutPosition != RecyclerView.NO_POSITION) {
-                itemView.findViewById<TextView>(R.id.descriptionTextView).text =
-                    data.first.someDescription
-                itemView.findViewById<ImageView>(R.id.wikiImageView).setOnClickListener {
-                    onListItemClickListener.onItemClick(data.first)
-                }
+            val combinedChange =
+                createCombinedPayload(payloads as List<Change<Pair<Data, Boolean>>>)
+            val oldData = combinedChange.oldData
+            val newData = combinedChange.newData
+            if (newData.first.someText != oldData.first.someText) {
+                holder.itemView.findViewById<TextView>(R.id.marsTextView).text =
+                    newData.first.someText
             }
         }
-    }
+}
 
-    fun appendItem() {
-        data.add(generateItem())
-        notifyItemInserted(itemCount - 1)
-
-    }
-
-    private fun generateItem() = Pair(Data(TYPE_MARS, 0,"Mars", ""), false)
-    inner class MarsViewHolder(view: View) : BaseViewHolder(view), ItemTouchHelperViewHolder {
-        override fun bind(data: Pair<Data, Boolean>) {
-            itemView.findViewById<ImageView>(R.id.marsImageView).setOnClickListener {
+inner class EarthViewHolder(view: View) : BaseViewHolder(view) {
+    override fun bind(data: Pair<Data, Boolean>) {
+        if (layoutPosition != RecyclerView.NO_POSITION) {
+            itemView.findViewById<TextView>(R.id.descriptionTextView).text =
+                data.first.someDescription
+            itemView.findViewById<ImageView>(R.id.wikiImageView).setOnClickListener {
                 onListItemClickListener.onItemClick(data.first)
             }
+        }
+    }
+}
 
-            itemView.findViewById<ImageView>(R.id.addItemImageView).setOnClickListener {
-                addItem()
-            }
-            itemView.findViewById<ImageView>(R.id.removeItemImageView).setOnClickListener {
-                removeItem()
-            }
+fun appendItem() {
+    data.add(generateItem())
+    notifyItemInserted(itemCount - 1)
 
-            itemView.findViewById<ImageView>(R.id.moveItemDown).setOnClickListener {
-                moveDown()
-            }
-            itemView.findViewById<ImageView>(R.id.moveItemUp).setOnClickListener {
-                moveUp()
-            }
+}
 
-            itemView.findViewById<TextView>(R.id.marsDescriptionTextView).visibility =
-                if (data.second) View.VISIBLE else View.GONE
-            itemView.findViewById<TextView>(R.id.marsTextView).setOnClickListener {
-                toggleText()
-            }
-            itemView.findViewById<ImageView>(R.id.dragHandleImageView).setOnTouchListener { _, event ->
-                if (event.actionMasked == MotionEvent.ACTION_DOWN) {
-                    dragListener.onStartDrag(this)
-                }
-                false
-            }
-
+private fun generateItem() = Pair(Data(TYPE_MARS, 0, "Mars", ""), false)
+inner class MarsViewHolder(view: View) : BaseViewHolder(view), ItemTouchHelperViewHolder {
+    override fun bind(data: Pair<Data, Boolean>) {
+        itemView.findViewById<ImageView>(R.id.marsImageView).setOnClickListener {
+            onListItemClickListener.onItemClick(data.first)
         }
 
-        private fun toggleText() {
-            data[layoutPosition] = data[layoutPosition].let {
-                it.first to !it.second
+        itemView.findViewById<ImageView>(R.id.addItemImageView).setOnClickListener {
+            addItem()
+        }
+        itemView.findViewById<ImageView>(R.id.removeItemImageView).setOnClickListener {
+            removeItem()
+        }
+
+        itemView.findViewById<ImageView>(R.id.moveItemDown).setOnClickListener {
+            moveDown()
+        }
+        itemView.findViewById<ImageView>(R.id.moveItemUp).setOnClickListener {
+            moveUp()
+        }
+
+        itemView.findViewById<TextView>(R.id.marsDescriptionTextView).visibility =
+            if (data.second) View.VISIBLE else View.GONE
+        itemView.findViewById<TextView>(R.id.marsTextView).setOnClickListener {
+            toggleText()
+        }
+        itemView.findViewById<ImageView>(R.id.dragHandleImageView).setOnTouchListener { _, event ->
+            if (event.actionMasked == MotionEvent.ACTION_DOWN) {
+                dragListener.onStartDrag(this)
             }
-            notifyItemChanged(layoutPosition)
+            false
         }
 
-        private fun moveUp() {
-            layoutPosition.takeIf { it > 1 }?.also { currentPosition ->
-                data.removeAt(currentPosition).apply {
-                    data.add(currentPosition - 1, this)
-                }
-                notifyItemMoved(currentPosition, currentPosition - 1)
+    }
+
+    private fun toggleText() {
+        data[layoutPosition] = data[layoutPosition].let {
+            it.first to !it.second
+        }
+        notifyItemChanged(layoutPosition)
+    }
+
+    private fun moveUp() {
+        layoutPosition.takeIf { it > 1 }?.also { currentPosition ->
+            data.removeAt(currentPosition).apply {
+                data.add(currentPosition - 1, this)
             }
-        }
-
-        private fun moveDown() {
-            layoutPosition.takeIf { it < data.size - 1 }?.also { currentPosition ->
-                data.removeAt(currentPosition).apply {
-                    data.add(currentPosition + 1, this)
-                }
-                notifyItemMoved(currentPosition, currentPosition + 1)
-            }
-        }
-
-        private fun addItem() {
-            data.add(layoutPosition, generateItem())
-            notifyItemInserted(layoutPosition)
-        }
-
-        private fun removeItem() {
-            data.removeAt(layoutPosition)
-            notifyItemRemoved(layoutPosition)
-        }
-
-        override fun onItemSelected() {
-            itemView.setBackgroundColor(Color.LTGRAY)
-
-        }
-
-        override fun onItemClear() {
-            itemView.setBackgroundColor(Color.WHITE)
+            notifyItemMoved(currentPosition, currentPosition - 1)
         }
     }
 
-    inner class HeaderViewHolder(view: View) : BaseViewHolder(view) {
-
-        override fun bind(dataItem: Pair<Data, Boolean>) {
-            itemView.setOnClickListener {
-                // onListItemClickListener.onItemClick(data.first)
-                data[1] = Pair(Data(TYPE_MARS,0, "Jupiter", ""), false)
-                notifyItemChanged(1, Pair(Data(TYPE_MARS,0, "", ""), false))
+    private fun moveDown() {
+        layoutPosition.takeIf { it < data.size - 1 }?.also { currentPosition ->
+            data.removeAt(currentPosition).apply {
+                data.add(currentPosition + 1, this)
             }
+            notifyItemMoved(currentPosition, currentPosition + 1)
         }
     }
 
-    override fun onItemMove(fromPosition: Int, toPosition: Int) {
-        data.removeAt(fromPosition).apply {
-            data.add(
-                if (toPosition > fromPosition) toPosition - 1 else toPosition,
-                this
-            )
-        }
-        notifyItemMoved(fromPosition, toPosition)
+    private fun addItem() {
+        data.add(layoutPosition, generateItem())
+        notifyItemInserted(layoutPosition)
     }
 
-    override fun onItemDismiss(position: Int) {
-        data.removeAt(position)
-        notifyItemRemoved(position)
+    private fun removeItem() {
+        data.removeAt(layoutPosition)
+        notifyItemRemoved(layoutPosition)
     }
 
-    class DiffUtilCallback(
-        private var oldItems: List<Pair<Data, Boolean>>,
-        private var newItems: List<Pair<Data, Boolean>>
-    ) : DiffUtil.Callback() {
-        override fun getOldListSize(): Int = oldItems.size
-        override fun getNewListSize(): Int = newItems.size
-        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean
-                =
-            oldItems[oldItemPosition].first.id == newItems[newItemPosition].first.id
-        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int):
-                Boolean =
-            oldItems[oldItemPosition].first.someText ==
-                    newItems[newItemPosition].first.someText
-        override fun getChangePayload(oldItemPosition: Int, newItemPosition: Int): Any? {
-            val oldItem = oldItems[oldItemPosition]
-            val newItem = newItems[newItemPosition]
-            return Change(
-                oldItem,
-                newItem
-            )
+    override fun onItemSelected() {
+        itemView.setBackgroundColor(Color.LTGRAY)
+
+    }
+
+    override fun onItemClear() {
+        itemView.setBackgroundColor(Color.WHITE)
+    }
+}
+
+inner class HeaderViewHolder(view: View) : BaseViewHolder(view) {
+
+    override fun bind(dataItem: Pair<Data, Boolean>) {
+        itemView.setOnClickListener {
+            // onListItemClickListener.onItemClick(data.first)
+            data[1] = Pair(Data(TYPE_MARS, 0, "Jupiter", ""), false)
+            notifyItemChanged(1, Pair(Data(TYPE_MARS, 0, "", ""), false))
         }
     }
+}
+
+override fun onItemMove(fromPosition: Int, toPosition: Int) {
+    data.removeAt(fromPosition).apply {
+        data.add(
+            if (toPosition > fromPosition) toPosition - 1 else toPosition,
+            this
+        )
+    }
+    notifyItemMoved(fromPosition, toPosition)
+}
+
+override fun onItemDismiss(position: Int) {
+    data.removeAt(position)
+    notifyItemRemoved(position)
+}
+
+class DiffUtilCallback(
+    private var oldItems: List<Pair<Data, Boolean>>,
+    private var newItems: List<Pair<Data, Boolean>>
+) : DiffUtil.Callback() {
+    override fun getOldListSize(): Int = oldItems.size
+    override fun getNewListSize(): Int = newItems.size
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+        oldItems[oldItemPosition].first.id == newItems[newItemPosition].first.id
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int):
+            Boolean =
+        oldItems[oldItemPosition].first.someText ==
+                newItems[newItemPosition].first.someText
+
+    override fun getChangePayload(oldItemPosition: Int, newItemPosition: Int): Any? {
+        val oldItem = oldItems[oldItemPosition]
+        val newItem = newItems[newItemPosition]
+        return Change(
+            oldItem,
+            newItem
+        )
+    }
+}
 
 }
