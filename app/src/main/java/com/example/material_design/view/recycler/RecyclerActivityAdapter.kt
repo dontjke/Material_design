f7f7d54a5e69be32c0947494d0ec892b8740913d
Romajke <skuaall@mail.ru>
2023-05-30 20:29:45 +0300

    добавил DiffUtilCallback Сравниваею id, затем контент если разный, возвращаю экземпляр класса Change, который и попадает в payload

--- app/src/main/java/com/example/material_design/view/recycler/RecyclerActivityAdapter.kt
+++ app/src/main/java/com/example/material_design/view/recycler/RecyclerActivityAdapter.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.material_design.R
 import com.example.material_design.view.recycler.Data.Companion.TYPE_EARTH
@@ -190,4 +191,28 @@ class RecyclerActivityAdapter(
         data.removeAt(position)
         notifyItemRemoved(position)
     }
+
+    class DiffUtilCallback(
+        private var oldItems: List<Pair<Data, Boolean>>,
+        private var newItems: List<Pair<Data, Boolean>>
+    ) : DiffUtil.Callback() {
+        override fun getOldListSize(): Int = oldItems.size
+        override fun getNewListSize(): Int = newItems.size
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean
+                =
+            oldItems[oldItemPosition].first.id == newItems[newItemPosition].first.id
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int):
+                Boolean =
+            oldItems[oldItemPosition].first.someText ==
+                    newItems[newItemPosition].first.someText
+        override fun getChangePayload(oldItemPosition: Int, newItemPosition: Int): Any? {
+            val oldItem = oldItems[oldItemPosition]
+            val newItem = newItems[newItemPosition]
+            return Change(
+                oldItem,
+                newItem
+            )
+        }
+    }
+
 }
