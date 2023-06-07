570600f9bf4069616068a0adfc773146352d3d58
Romajke <skuaall@mail.ru>
2023-06-07 14:42:40 +0300

    решение для визуализации загрузки/обработки данных через LinearProgressIndicator

--- app/src/main/java/com/example/material_design/view/ux/SplashActivity.kt
+++ app/src/main/java/com/example/material_design/view/ux/SplashActivity.kt
@@ -1,29 +1,32 @@
 package com.example.material_design.view.ux
 
-import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.view.View
-import android.widget.ImageView
+import android.os.CountDownTimer
 import androidx.appcompat.app.AppCompatActivity
 import com.example.material_design.MainActivity
 import com.example.material_design.R
-import com.example.material_design.view.picture.PictureOfTheDayFragment
+import com.google.android.material.progressindicator.LinearProgressIndicator
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        findViewById<ImageView>(R.id.image_view).animate().rotation(720f).setDuration(2000L).start()
+        val progress = findViewById<LinearProgressIndicator>(R.id.progress)
+        val fullTime = 5000f
+        object : CountDownTimer(fullTime.toLong(), 1L) {
+            override fun onTick(millisUntilFinished: Long) {
+                val process = ((1 - millisUntilFinished / fullTime) * 100).toInt()
+                if (progress.progress != process)
+                    progress.progress = process
+            }
 
-        ObjectAnimator.ofFloat(findViewById<ImageView>(R.id.image_view),View.ROTATION,720f).setDuration(2000L).start()
+            override fun onFinish() {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                finish()
+            }
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this,MainActivity::class.java))
-            finish()
-        },2000L)
+        }.start()
     }
 }
