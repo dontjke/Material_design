0aad468dd30f7ac3155d9ffe62101f119f04c9e2
Romajke <skuaall@mail.ru>
2023-06-07 13:59:35 +0300

    добавил в MainActivity код для задержки отображения экрана загрузки и анимацию добавил SplashActivity.kt

---
+++ app/src/main/java/com/example/material_design/view/ux/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.material_design.view.ux
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.material_design.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
