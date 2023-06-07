bbc8671c990523316c1e256664c3889d3067a2e0
Romajke <skuaall@mail.ru>
2023-06-07 14:20:14 +0300

    решение для визуализации загрузки/обработки данных на старте(и не только)

--- app/src/main/java/com/example/material_design/MainActivity.kt
+++ app/src/main/java/com/example/material_design/MainActivity.kt
@@ -1,14 +1,7 @@
 package com.example.material_design
 
-import android.animation.ObjectAnimator
-import android.os.Build
 import android.os.Bundle
-import android.os.CountDownTimer
-import android.view.View
-import android.view.ViewTreeObserver
-import android.view.animation.AnticipateInterpolator
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.animation.doOnEnd
 import androidx.core.view.WindowCompat
 import com.example.material_design.databinding.ActivityMainBinding
 import com.example.material_design.view.picture.PictureOfTheDayFragment
@@ -30,41 +23,5 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.container, PictureOfTheDayFragment.newInstance())
                 .commit()
         }
-        var isHideSplashScreen = false
-        object : CountDownTimer(5000, 1000) {
-            override fun onTick(millisUntilFinished: Long) {}
-            override fun onFinish() {
-                isHideSplashScreen = true
-            }
-        }.start()
-        val content: View = findViewById(android.R.id.content)
-        content.viewTreeObserver.addOnPreDrawListener(
-            object : ViewTreeObserver.OnPreDrawListener {
-                override fun onPreDraw(): Boolean {
-                    return if (isHideSplashScreen) {
-                        content.viewTreeObserver.removeOnPreDrawListener(this)
-                        true
-                    } else {
-                        false
-                    }
-                }
-            })
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            splashScreen.setOnExitAnimationListener { splashScreenView ->
-                val slideLeft = ObjectAnimator.ofFloat(
-                    splashScreenView,
-                    View.TRANSLATION_X,
-                    0f,
-                -splashScreenView.height.toFloat()
-                )
-                slideLeft.interpolator = AnticipateInterpolator()
-                slideLeft.duration = 1000L
-                slideLeft.doOnEnd { splashScreenView.remove() }
-                slideLeft.start()
-            }
-        }
-
-
     }
 }
--- app/src/main/java/com/example/material_design/view/ux/SplashActivity.kt
+++ app/src/main/java/com/example/material_design/view/ux/SplashActivity.kt
@@ -1,12 +1,29 @@
 package com.example.material_design.view.ux
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.ObjectAnimator
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.material_design.MainActivity
 import com.example.material_design.R
+import com.example.material_design.view.picture.PictureOfTheDayFragment
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        findViewById<ImageView>(R.id.image_view).animate().rotation(720f).setDuration(2000L).start()
+
+        ObjectAnimator.ofFloat(findViewById<ImageView>(R.id.image_view),View.ROTATION,720f).setDuration(2000L).start()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        },2000L)
     }
 }
